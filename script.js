335f9c24be11d06141a8781c6a123f2a58b4e384
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2023-11-22 21:02:13 -0700

    Added Click and Drag functionality, validation on grid creation. Fixed bug that was allowing column divs to be dragged.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// init
+createDrawingPad(16, 16);
+
+let onMouseDown = false;
+
+let isShaderMode = true;
+let isRainbowMode = false;
+let isColorMode = false;
+let isEraserMode = false;
+
+////// Functions
 function createDrawingPad(xAxis, yAxis) {
     let container = document.querySelector("#container");
     removeAllChildNodes(container);
@@ -5,11 +16,11 @@ function createDrawingPad(xAxis, yAxis) {
         let columnContainer = document.createElement("div");
         columnContainer.className = "column"
         columnContainer.id = `column${i}`;
+        columnContainer.ondragstart = () => false;
         for (let j = 1; j <= yAxis; j++) {
             let box = document.createElement("div");
             box.id = `column${i}:row${j}`;
             box.className = "box box-shade-0";
-            
             addBoxEvents(box);
             columnContainer.appendChild(box);
         }
@@ -17,50 +28,103 @@ function createDrawingPad(xAxis, yAxis) {
     }
 }
 
-function incrementBackgroundColor(classList) {
-   let boxClassList = classList;
-   if (boxClassList.contains("box-shade-0")) {
-        boxClassList.replace("box-shade-0", "box-shade-10");
-   } else if (boxClassList.contains("box-shade-10")) {
-        boxClassList.replace("box-shade-10", "box-shade-20");
-   } else if (boxClassList.contains("box-shade-20")) {
-        boxClassList.replace("box-shade-20", "box-shade-30");
-   } else if (boxClassList.contains("box-shade-30")) {
-        boxClassList.replace("box-shade-30", "box-shade-40");
-   } else if (boxClassList.contains("box-shade-40")) {
-        boxClassList.replace("box-shade-40", "box-shade-50");
-   } else if (boxClassList.contains("box-shade-50")) {
-        boxClassList.replace("box-shade-50", "box-shade-60");
-   } else if (boxClassList.contains("box-shade-60")) {
-        boxClassList.replace("box-shade-60", "box-shade-70");
-   } else if (boxClassList.contains("box-shade-70")) {
-        boxClassList.replace("box-shade-70", "box-shade-80");
-   } else if (boxClassList.contains("box-shade-80")) {
-        boxClassList.replace("box-shade-80", "box-shade-90");
-   } else if (boxClassList.contains("box-shade-90")) {
-        boxClassList.replace("box-shade-90", "box-shade-100");
-   } else if (boxClassList.contains("box-shade-100")) {
-        // do nothing 
-   }
+function createNewDrawingPad() {
+    let xAxis = document.querySelector("#x-axis");
+    let yAxis = document.querySelector("#y-axis");
 
-   return boxClassList;
+    if (isValidGridSize(xAxis.value, yAxis.value)) {
+        document.querySelector("#warning-message").innerHTML = "";
+        createDrawingPad(xAxis.value, yAxis.value);
+    } else {
+        xAxis.value = "";
+        yAxis.value = "";
+        document.querySelector("#warning-message").innerHTML = "Invalid Grid Size"
+    }
 }
 
+function incrementBackgroundColor(classList) {
+    let boxClassList = classList;
+    if (boxClassList.contains("box-shade-0")) {
+         boxClassList.replace("box-shade-0", "box-shade-10");
+    } else if (boxClassList.contains("box-shade-10")) {
+         boxClassList.replace("box-shade-10", "box-shade-20");
+    } else if (boxClassList.contains("box-shade-20")) {
+         boxClassList.replace("box-shade-20", "box-shade-30");
+    } else if (boxClassList.contains("box-shade-30")) {
+         boxClassList.replace("box-shade-30", "box-shade-40");
+    } else if (boxClassList.contains("box-shade-40")) {
+         boxClassList.replace("box-shade-40", "box-shade-50");
+    } else if (boxClassList.contains("box-shade-50")) {
+         boxClassList.replace("box-shade-50", "box-shade-60");
+    } else if (boxClassList.contains("box-shade-60")) {
+         boxClassList.replace("box-shade-60", "box-shade-70");
+    } else if (boxClassList.contains("box-shade-70")) {
+         boxClassList.replace("box-shade-70", "box-shade-80");
+    } else if (boxClassList.contains("box-shade-80")) {
+         boxClassList.replace("box-shade-80", "box-shade-90");
+    } else if (boxClassList.contains("box-shade-90")) {
+         boxClassList.replace("box-shade-90", "box-shade-100");
+    } else if (boxClassList.contains("box-shade-100")) {
+         // do nothing 
+    }
+    return boxClassList;
+ }
+
+function draw(classList) {
+    if(isColorMode) {
+
+    }
+    if (isRainbowMode) {
+
+    }
+    if (isShaderMode) {
+        return incrementBackgroundColor(classList);
+    }
+    if (isEraserMode) {
+
+    }
+}
+
+//// Validation
+function isValidGridSize(xAxis, yAxis) {
+    // Is a number
+    if (isNaN(xAxis) && isNaN(yAxis)) return false;
+    // Number must be greater than 0
+    if (xAxis <= 0 || yAxis <= 0) return false;
+    // Number must be less than or equal to 100
+    if (xAxis > 100 || yAxis > 100) return false;
+
+    return true;
+}
+
+//// Events
+let setGridSizeButton = document.querySelector("#setGridSizeButton");
+setGridSizeButton.addEventListener("click", createNewDrawingPad);
+
+// Mirror x-axis input onto y-axis
+let xAxisInput = document.querySelector("#x-axis");
+xAxisInput.addEventListener("input", function(e) {
+    let xAxisInputValue = e.target.value;
+    let yAxisInput = document.querySelector("#y-axis");
+    yAxisInput.value = xAxisInputValue;
+})
+
 function addBoxEvents(box) {
+    box.addEventListener("mousedown", function(e) {
+        onMouseDown = true;
+        box.className = draw(e.target.classList);
+    });
     box.addEventListener("mouseenter", function(e) {
-        console.log(e.target);
-        let box = e.target;
-        box.className = incrementBackgroundColor(box.classList);
-        
+        if(onMouseDown) {
+            box.className = draw(e.target.classList);
+        }
+    });
+    box.addEventListener("mouseup", function(e) {
+        onMouseDown = false;
     });
 }
 
-function createNewDrawingPad() {
-    let xAxis = document.querySelector("#x-axis").value;
-    let yAxis = document.querySelector("#y-axis").value;
-    createDrawingPad(xAxis, yAxis)
-}
-
+//// Helper methods
 // via: https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -68,9 +132,3 @@ function removeAllChildNodes(parent) {
     }
 }
 
-// do stuff
-let setGridSizeButton = document.querySelector("#setGridSizeButton");
-setGridSizeButton.addEventListener("click", createNewDrawingPad);
-
-
-createDrawingPad(16, 16);
